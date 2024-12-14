a276f2963ef0a810166a6d2f2b10409d90481a17
W0Ifyyy <bartosz.wilk.123@hotmail.com>
2024-12-14 18:47:50 +0100

    learned how to implement one to one, one to many relations

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './typeorm/entities/User';
 import { UsersModule } from './users/users.module';
+import { Profile } from './typeorm/entities/Profile';
+import { Post } from './typeorm/entities/Post';
 
 @Module({
   imports: [
@@ -13,7 +15,7 @@ import { UsersModule } from './users/users.module';
       port: 3306,
       username: 'root',
       database: 'nestjs_mysql_tutorial',
-      entities: [User],
+      entities: [User, Profile, Post],
       synchronize: true,
       password: '',
     }),
--- src/users/controller/users/users.controller.ts
+++ src/users/controller/users/users.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   Param,
   ParseIntPipe,
@@ -8,8 +9,11 @@ import {
   Put,
 } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
+import { CreateUserPostDto } from 'src/users/dtos/CreateUserPost.dto';
+import { CreateUserProfileDto } from 'src/users/dtos/CreateUserProfile.dto';
 import { UpdateUserDto } from 'src/users/dtos/UpdateUser.dto';
 import { UsersService } from 'src/users/services/users/users.service';
+import { CreateUserPostParams } from 'src/utils/types';
 
 @Controller('users')
 export class UsersController {
@@ -25,10 +29,30 @@ export class UsersController {
     return response;
   }
   @Put(':id')
-  updateUserById(
+  async updateUserById(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateUserDto: UpdateUserDto,
   ) {
-    this.userService.updateUserById(updateUserDto, id);
+    await this.userService.updateUserById(updateUserDto, id);
+  }
+
+  @Delete(':id')
+  async deleteUserById(@Param('id', ParseIntPipe) id: number) {
+    await this.userService.deleteUser(id);
+  }
+
+  @Post(':id/profiles')
+  createUserProfile(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() createUserProfileDto: CreateUserProfileDto,
+  ) {
+    return this.userService.createUserProfile(id, createUserProfileDto);
+  }
+  @Post(':id/posts')
+  createUserPost(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() createUserPostDto: CreateUserPostDto,
+  ) {
+    return this.userService.createUserPost(id, createUserPostDto);
   }
 }
